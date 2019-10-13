1e761745f3b5763a5f22cea4a7367e6a95560df5
rychtovy24 <rychtovy@gmail.com>
2019-10-13 09:29:13 +0200

    [TASK] evaluate step and check finish

--- minisoft1/Main.py
+++ minisoft1/Main.py
@@ -4,6 +4,7 @@ import sys
 from PIL import Image
 from PIL import ImageTk
 import os
+from functools import partial
 
 PATH_IMAGES = './images/'
 PATH_RULES = PATH_IMAGES + 'rules/'
@@ -14,6 +15,19 @@ class Node:
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
+
+    def equals(self, node):
+        pom = self
+        while True:
+            if pom is None and node is None:
+                return True
+            if pom is None or node is None:
+                return False
+            if pom.data.char_name != node.data.char_name:
+                return False
+            pom = pom.next_node
+            node = node.next_node
+
 
 
 class Character:
@@ -103,7 +117,7 @@ class Main:
 
         self.burger = [ImageTk.PhotoImage(Image.open(PATH_IMAGES + 'burger_top.png')),
                         ImageTk.PhotoImage(Image.open(PATH_IMAGES + 'burger_bottom.png'))]
-        self.root = None
+        self.my_word = None
         with open(file_name, 'r') as file:
             row = file.readline().strip()
 
@@ -129,7 +143,7 @@ class Main:
         y = 10
         for key in self.rules:
             rule = self.rules[key]
-            self.button_rules.append(Button(master=self.canvas_left, command= lambda: self.apply_rule(key), image=rule.image))
+            self.button_rules.append(Button(master=self.canvas_left, command=partial(self.apply_rule,rule.name), image=rule.image))
             self.button_rules[-1].place(x=x,y=y)
             y += rule.height+10
         initY = y
@@ -161,10 +175,13 @@ class Main:
         characters = word1.split(',')
         self.start = Node(self.characters[characters[0]])
         pom = self.start
+        self.my_word = Node(self.characters[characters[0]])
+        pom2 = self.my_word
         for c in characters[1:]:
             pom.next_node = Node(self.characters[c])
             pom = pom.next_node
-
+            pom2.next_node = Node(self.characters[c])
+            pom2 = pom2.next_node
         characters = word2.split(',')
         self.goal = Node(self.characters[characters[0]])
         pom = self.goal
@@ -173,7 +190,52 @@ class Main:
             pom = pom.next_node
 
     def apply_rule(self, key):
-        pass
+        pom = self.my_word
+        rule = self.rules[key]
+        while pom is not None:
+            if pom.data.char_name == rule.search_characters[0].char_name:
+                temp = pom
+                first_node = pom
+                last_node = None
+                is_equal = True
+                for i in rule.search_characters:
+                    is_equal = is_equal and (temp.data.char_name == i.char_name)
+                    temp = temp.next_node
+                    last_node = temp
+                if is_equal:
+                    print('tu')
+                    pom.data = self.characters[rule.replace_characters[0].char_name]
+                    for i in rule.replace_characters[1:]:
+                        pom.next_node = Node(self.characters[i.char_name])
+                        pom = pom.next_node
+                    pom.next_node = last_node
+                    break
+            pom = pom.next_node
+        pom = self.my_word
+        while pom is not None:
+            print(pom.data.char_name,end=' ')
+            pom = pom.next_node
+        print()
+        self.print_next_step()
+        self.check_if_end()
+
+    def check_if_end(self):
+        if self.my_word.equals(self.goal):
+            print('VYHRAL')
+
+    def print_next_step(self):
+        x = 10
+        y = 10
+        pom = self.my_word
+        self.canvas_right.delete('all')
+        self.canvas_right.create_image(x, y, image=self.burger[0], anchor=NW)
+        y += Character.HEIGHT
+        while pom is not None:
+            self.canvas_right.create_image(x, y, image=pom.data.image, anchor=NW)
+            y += Character.HEIGHT
+            pom = pom.next_node
+        self.canvas_right.create_image(x, y, image=self.burger[1], anchor=NW)
+
 
 
 if __name__ == '__main__':
